d492cbd5021867f9b2d4c5b4489c42ea6f2e2717
Wangheda <whd.thu@gmail.com>
2012-03-31 01:36:00 +0800

    Able to run

--- search-lib/menus.js
+++ search-lib/menus.js
@@ -1,24 +1,73 @@
-fetch_content = [];
-DayHourList = [];
-pagenum = 40;
-
-function genericOnClick(info, tab) {
-    uid = info.linkUrl.match("id=[0-9]+")[0];
-    uid = uid.substring(3,uid.length)
-    chrome.tabs.create({url:"punchcard.html"}, function(tab){
-        chrome.tabs.sendRequest(tab.id, {agent:"renren-punchcard", user:uid}, function(response){
-            if (respose.agent && response.agent=="renren-punchcard") {
-                console.log(response.message);
-            }
-        });
-    });
+menuID2getGroupID = {}
+function getSetGroup() {
+	var setGroup;
+	if(!localStorage.getItem("setGroup")) {
+		setGroup = new Array(getGroup.length);
+		for (var i=0; i<getGroup.length; i++) {
+			setGroup[i] = false;
+		}
+		localStorage["setGroup"] = setGroup;
+	} else {
+		setGroup = localStorage.getItem("setGroup");
+		setGroup = setGroup.split(',');
+		for (var i=0; i<setGroup.length; i++) {
+			if (setGroup[i] == "true") {
+				setGroup[i] = true;
+			} else {
+				setGroup[i] = false;
+			}
+		}
+	}
+	return setGroup;
 }
 
-var property = {"title": "Show PunchCard", 
-"contexts":["link"], 
-"targetUrlPatterns":["http://www.renren.com/profile.do*"], 
-"documentUrlPatterns":["http://*.renren.com/*"],
-"onclick": genericOnClick};
+function changeMenus() {
+	setGroup = getSetGroup();
+	number = 0;
+	for (var i=0; i<setGroup.length; i++) {
+		if (setGroup[i]) {
+			var text = getGroup[i].text;
+			var property = {"title": "在 "+text+" 中搜索", 
+				"contexts":["selection"], 
+				"onclick": genericOnClick};
+			var mid = chrome.contextMenus.create(property);
+			menuID2getGroupID[mid] = i;
+			number ++;
+		}
+	}
+	if (number == 0) {
+		var property = {"title": "设置搜索图书馆", 
+			"contexts":["selection"], 
+			"onclick": genericOnClick};
+		var mid = chrome.contextMenus.create(property);
+		menuID2getGroupID[mid] = -1;//means no lib are chosen
+	}
+}
 
 
-var mid = chrome.contextMenus.create(property);
+function genericOnClick(info, tab) {
+	console.log(info);
+	getGroupID = menuID2getGroupID[info.menuItemId];
+	if (getGroupID >= 0) {
+		// normal
+		this_url = getGroup[getGroupID].url.replace(/%TITLE%/g, info.selectionText);
+		chrome.tabs.create({url:this_url});
+	} else {
+		// no lib chosen
+		chrome.tabs.create({url:'option.html'});
+	}
+}
+
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
+	if (request.host == "searchlib") {
+		sendResponse({host:"searchlib", message:"gotit"});
+		if (request.message == "setGroupChanged") {
+			chrome.contextMenus.removeAll();
+			changeMenus();
+		}
+	}
+});
+
+// change Menus
+changeMenus();
--- search-lib/option.js
+++ search-lib/option.js
@@ -1,21 +1,44 @@
 // JavaScript Document
 $(document).ready(function(){
 	HTML = "";
+	var setGroup;
+	if(!localStorage.getItem("setGroup")) {
+		setGroup = new Array(getGroup.length);
+		for (var i=0; i<getGroup.length; i++) {
+			setGroup[i] = false;
+		}
+		localStorage["setGroup"] = setGroup;
+	} else {
+		setGroup = localStorage.getItem("setGroup");
+		setGroup = setGroup.split(',');
+		for (var i=0; i<setGroup.length; i++) {
+			if (setGroup[i] == "true") {
+				setGroup[i] = true;
+			} else {
+				setGroup[i] = false;
+			}
+		}
+	}
 	for (var i=0; i<getGroup.length; i++) {
-		HTML += "<p><input type='checkbox' class='set' num='"+getGroup[i].num.toString()+"' name='check"+getGroup[i].num.toString()+"' id='check"+getGroup[i].num.toString()+"'onchange='settings()'/><label>"+getGroup[i].text+"</label></p>";
+		if (setGroup[getGroup[i].num-1]) {
+			HTML += "<p><input type='checkbox' class='set' num='"+getGroup[i].num.toString()+"' name='check"+getGroup[i].num.toString()+"' id='check"+getGroup[i].num.toString()+"'onchange='settings()' checked='checked'/><label>"+getGroup[i].text+"</label></p>";
+		} else {
+			HTML += "<p><input type='checkbox' class='set' num='"+getGroup[i].num.toString()+"' name='check"+getGroup[i].num.toString()+"' id='check"+getGroup[i].num.toString()+"'onchange='settings()'/><label>"+getGroup[i].text+"</label></p>";
+		}
 	}
 	$('#lib').html(HTML)
 });
 
 function settings() {
 	console.log('enter setting')
-	var List = $('.set');
+	var List = $('.set').toArray();
 	var setGroup = new Array(List.length);
 	if(List) {
 		for (var i=0; i<List.length; i++) {
-			index = parseInt(List[i].num);
-			setGroup[index] = List[i].checked;
+			index = parseInt(List[i].name.substr(5,7));
+			setGroup[index-1] = List[i].checked;
 		}
 	}
 	localStorage["setGroup"] = setGroup;
+	chrome.extension.sendRequest({host:"searchlib", message:"setGroupChanged"}, function(response){console.log(response);})
 }
