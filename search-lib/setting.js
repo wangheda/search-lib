c33a31e7c3235eb3152914005fe80f0051254596
Wangheda <whd.thu@gmail.com>
2012-03-31 12:07:26 +0800

    Add beihang

--- search-lib/setting.js
+++ search-lib/setting.js
@@ -6,4 +6,5 @@ getGroup = [ {num:1, text:"清华大学图书馆", url:"http://innopac.lib.tsing
 {num:5, text:"中国科技大学图书馆", url:"http://opac.lib.ustc.edu.cn/opac/search_adv_result.php?keyword=%TITLE%&option=book_name&ndo=search&type=book&q0=%TITLE%&sType0=02&class=all&key=%TITLE%&rd_book=opac"},
 {num:6, text:"厦门大学图书馆", url:"http://210.34.4.28/opac/openlink.php?historyCount=1&strText=%TITLE%&doctype=ALL&strSearchType=title&match_flag=forward&displaypg=20&sort=CATA_DATE&orderby=desc&showmode=list&dept=ALL"},
 {num:7, text:"四川大学图书馆", url:"http://202.115.54.13:8080/F/-/?func=find-b&find_code=WRD&request=%TITLE%&filter_code_1=WLN&filter_request_1=&filter_code_2=WYR&filter_request_2=&filter_code_3=WYR&filter_request_3=&filter_code_4=WFM&filter_request_4=&filter_code_5=WSL&filter_request_5=&con_lng=chi"},
-{num:8, text:"国家图书馆", url:"http://opac.nlc.gov.cn/F/?request=%TITLE%&func=find-m&find_code=WRD&FIND_BASE=NLC01&FIND_BASE=NLC09"}];
+{num:8, text:"国家图书馆", url:"http://opac.nlc.gov.cn/F/?request=%TITLE%&func=find-m&find_code=WRD&FIND_BASE=NLC01&FIND_BASE=NLC09"},
+{num:9, text:"北京航天航空大学图书馆", url:"http://202.112.134.140:8080/opac/openlink.php?historyCount=1&strText=%TITLE%&doctype=ALL&strSearchType=title&match_flag=forward&displaypg=10&sort=CATA_DATE&orderby=desc&dept=ALL"}];
